a2ffbedcfdf70b99da63bc2b23df40f5a4db15fb
pinto894 <leticiapinto894@gmail.com>
2024-10-02 21:30:38 -0300

    Add files via upload

--- AULA_01/exemplo_4.py
+++ AULA_01/exemplo_4.py
@@ -1,5 +1,5 @@
 # calcular a idade de uma pessoa a partir do ano do nascimento e agora aparecendo o nome 
-n2 = input("informe o seu nome: ")
-n1 = int(input("Informe o seu ano de nascimento: "))
-resultado = 2024 - n1
-print(n2,"a sua idade é: ", resultado)
+nome = input("informe o seu nome: ")
+ano_nasc = int(input("Informe o seu ano de nascimento: "))
+idade = 2024 - ano_nasc
+print("Sr(a) ", nome, "a sua idade é: ",idade," anos.")
